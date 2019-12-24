ccc65eb98e209e85c0845f89a74003a8145d7bea
Pankaj <pgrover2079@gmail.com>
2019-12-23 17:46:34 -0800

    Without VS Code

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,14 @@
-const person = () => {
+import React from 'react';
 
+const person = () => {
+  return (
+    <p>
+      This is first para
+    </p>
+  )
 }
+
+if (a < 2) {
+  console.log("this");
+}
+
